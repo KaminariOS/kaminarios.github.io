ab4647f79a4f5e6e2fbeddeb3a346cfc604d9905
Kaminari <chankocyo@gmail.com>
2022-03-04 09:32:28 +0800

    Add Image component for posting.

---
+++ src/lib/Image.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let src;
+    export let alt;
+    export let caption = true;
+</script>
+
+<figure>
+    <img {src} {alt}/>
+    {#if caption}
+        <figcaption>
+            {alt}
+        </figcaption>
+    {/if}
+</figure>
+
+<style lang="scss">
+  figure {
+    display: flex;
+    flex-flow: column;
+  }
+  img {
+    max-width: 100%;
+    max-height: 50vh;
+    margin: auto;
+    border-radius: 10px;
+    display: block;
+  }
+  figcaption {
+    font: italic smaller "Open Sans", "DejaVu Sans", sans-serif;
+    padding: 3px;
+    text-align: center;
+  }
+
+</style>
